79fafd39655cdff38d556d56edb1fd224aea6202
陈豪恩 <73596300+chenhaoen@users.noreply.github.com>
2023-05-29 21:47:39 +0800

    增加windows上构建工程的脚本

---
+++ windows.bat
@@ -0,0 +1,6 @@
+set OSG_DIR=C:\Users\16000\source\osg\osg-dynamic-sdk
+set OSG_INCLUDE_DIR=%OSG_DIR%/include
+set CMAKE_BIN_DIR=C:\Program Files\CMake\bin
+set QT6_DIR=C:\Qt\6.5.0\msvc2019_64
+
+"%CMAKE_BIN_DIR%/cmake-gui.exe"
